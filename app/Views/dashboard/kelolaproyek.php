ce4199ecbd83e5d87607e85616d081f8120df6f7
fajarskak <fajarskak19@gmail.com>
2022-07-13 02:39:58 +0700

    mengubah dashboard bagian konten

--- app/Views/dashboard/kelolaproyek.php
+++ app/Views/dashboard/kelolaproyek.php
@@ -70,51 +70,6 @@
                 </ul>
             </div>
         </nav>
-        <div class="container-fluid px-4">
-            <div class="row g-3 my-2">
-                <div class="col-md-3">
-                    <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
-                        <div>
-                            <h3 class="fs-2">720</h3>
-                            <p class="fs-5">Menu 1</p>
-                        </div>
-                        <i class="fas fa-gift fs-1 primary-text border rounded-full secondary-bg p-3"></i>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
-                        <div>
-                            <h3 class="fs-2">4920</h3>
-                            <p class="fs-5">Menu 2</p>
-                        </div>
-                        <i class="fas fa-hand-holding-usd fs-1 primary-text border rounded-full secondary-bg p-3"></i>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
-                        <div>
-                            <h3 class="fs-2">3899</h3>
-                            <p class="fs-5">Menu 3</p>
-                        </div>
-                        <i class="fas fa-truck fs-1 primary-text border rounded-full secondary-bg p-3"></i>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
-                        <div>
-                            <h3 class="fs-2">%25</h3>
-                            <p class="fs-5">Menu 3</p>
-                        </div>
-                        <i class="fas fa-chart-line fs-1 primary-text border rounded-full secondary-bg p-3"></i>
-                    </div>
-                </div>
-            </div>
-
-
-
-        </div>
+    
     </div>
     <?= $this->endSection(); ?>
