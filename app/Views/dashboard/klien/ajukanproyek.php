93d4678f43b15a967e91bc67d4d305f24bf7a6cc
denydemang <denyk819@gmail.com>
2022-07-17 12:13:45 +0700

    mengubah halaman ajukan proyek pada dashboard klien

--- app/Views/dashboard/klien/ajukanproyek.php
+++ app/Views/dashboard/klien/ajukanproyek.php
@@ -1,40 +1,53 @@
 <?= $this->extend('dashboard/klien/template'); ?>
 <?= $this->section('dashboardklien'); ?>
-<div class="container-fluid w-60 px-5 py-5">
-    <div class="card bg-dark">
-        <div class="card-body bg-white">
-            <h3 class="card-title pb-4">Silakan Isi From Berikut Untuk Mengajukan Proyek</h3>
+<div class="container-fluid px-5 d-flex justify-content-center py-5">
+    <div class="card shadow w-50">
+        <div class="card-body p-4 bg-white">
+            <h3 class="card-title pb-4">Silakan Isi Form Berikut Untuk Mengajukan Proyek</h3>
             <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <div class="mb-3 row">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">Nama</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="staticEmail">
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-3 col-form-label">Alamat</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-3 col-form-label">No Telpon</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-3 col-form-label">Nama Proyek</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-3 col-form-label">Jenis Proyek</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-3 col-form-label">Lokasi Proyek</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-3 col-form-label">Catatan</label>
+                    <div class="col-sm-9">
+                        <textarea type="input" class="form-control" id="inputPassword"></textarea>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Ajukan</button>
             </form>
 
             <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
