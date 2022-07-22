2820efee6c7dd30c37bed643e645f80e78b3dfe2
denydemang <denyk819@gmail.com>
2022-07-22 13:02:10 +0700

    memodifikasi tampilan alert jika username atau email sudah terdaftar

--- app/Views/registrasi/index.php
+++ app/Views/registrasi/index.php
@@ -14,6 +14,31 @@
                         </div>
                         <div class="col-md-8 col-sm-12">
                             <form id="formregistrasi" action="<?= base_url(); ?>/registrasi/save" method="post" class="mx-1 mx-md-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                                    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                    </symbol>
+                                </svg>
+                                <!-- Pesan Message Ketika Berhasil buat Akun -->
+                                <?php if ($validation->hasError('email') && $validation->hasError('username')) : ?>
+                                    <div class="alert alert-danger p-1 d-flex align-items-center" style="height:50px;" role="alert">
+                                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                                            <use xlink:href="#exclamation-triangle-fill" />
+                                        </svg>
+                                        <div>
+                                            Email dan Username Sudah Terdaftar
+                                        </div>
+                                    </div>
+                                <?php elseif ($validation->hasError('email') || $validation->hasError('username')) : ?>
+                                    <div class="alert alert-danger p-1 d-flex align-items-center" style="height:50px;" role="alert">
+                                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                                            <use xlink:href="#exclamation-triangle-fill" />
+                                        </svg>
+                                        <div>
+                                            <?= $validation->getError('username'); ?><?= $validation->getError('email'); ?>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
                                 <?= csrf_field(); ?>
                                 <div class="row">
                                     <div class="col d-flex flex-row align-items-center mb-4">
@@ -26,9 +51,7 @@
                                         <div class="form-outline flex-fill mb-0">
                                             <label class="form-label" for="email">Email</label>
                                             <input type="email" class="form-control <?= ($validation->hasError('email')) ? 'is-invalid' : ''; ?>" name="email" autocomplete="off" value="<?= old('email'); ?>" />
-                                            <div class="invalid-feedback pesanerror">
-                                                <?= $validation->getError('email'); ?>
-                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
@@ -37,9 +60,6 @@
                                         <div class="form-outline flex-fill mb-0">
                                             <label class="form-label" for="username">Username</label>
                                             <input type="text" id="username" class="form-control <?= ($validation->hasError('username')) ? 'is-invalid' : ''; ?>" name="username" autocomplete="off" value="<?= old('username'); ?>" />
-                                            <div class="invalid-feedback pesanerror">
-                                                <?= $validation->getError('username'); ?>
-                                            </div>
                                         </div>
                                     </div>
                                     <div class="col d-flex flex-row align-items-center mb-4">
